2c795f53db3f9266715699ac58838542a98b883d
dsan22 <jankovicdusan00@gmail.com>
2025-04-13 18:14:26 +0200

    capitalized category name

--- src/app/recepie/recepie.component.ts
+++ src/app/recepie/recepie.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Recepie } from '../../types';
+import { CapitalizePipe } from '../pipes/capitalize.pipe';
 
 @Component({
   selector: 'app-recepie',
-  imports: [],
+  imports: [CapitalizePipe],
   templateUrl: './recepie.component.html',
   styleUrl: './recepie.component.css'
 })
