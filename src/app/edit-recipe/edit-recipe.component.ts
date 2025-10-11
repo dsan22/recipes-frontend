0efad166dc341fefbbe16f6ddd6bab47a7185cfa
Dusan Jankovic <jankovicdusan00@gmail.com>
2025-10-11 15:36:08 +0200

    created initial edit recipe component

---
+++ src/app/edit-recipe/edit-recipe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-recipe',
+  imports: [],
+  templateUrl: './edit-recipe.component.html',
+  styleUrl: './edit-recipe.component.css'
+})
+export class EditRecipeComponent {
+
+}
