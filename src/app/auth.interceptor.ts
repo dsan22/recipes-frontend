6b064a35e99c717bb2050069851c43a8e8dc069f
Dusan Jankovic <jankovicdusan00@gmail.com>
2025-10-20 22:07:30 +0200

    added auth interceptor for sending token

---
+++ src/app/auth.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { TokenService } from './services/token.service';
+
+export const AuthInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const authToken=inject(TokenService).getToken();
+  if(authToken){
+    const newReq=req.clone({
+      setHeaders:{
+        Authorization: `Bearer ${authToken}`,
+      }
+    });
+    return next(newReq)
+  }
+ 
+  return next(req);
+  
+};
