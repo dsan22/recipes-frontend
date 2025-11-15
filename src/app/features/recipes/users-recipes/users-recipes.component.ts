bdd7fd2618571d3d8a3430a4e31b37db0b8c5b91
Dusan Jankovic <jankovicdusan00@gmail.com>
2025-11-15 22:13:14 +0100

    added creating blank recipes

--- src/app/features/recipes/users-recipes/users-recipes.component.ts
+++ src/app/features/recipes/users-recipes/users-recipes.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RecipesService } from '../recipes.service';
 import { Router } from '@angular/router';
-import { Recipe, RecipeDetails } from '../../../../types';
+import { CreateBlankRecipeDTO, Recipe, RecipeDetails, User } from '../../../../types';
 import { CommonModule } from '@angular/common';
 import { RecipeComponent } from "../item/recipe.component";
+import { AuthService } from '../../../core/services/auth.service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-users-recipes',
@@ -12,14 +14,18 @@ import { RecipeComponent } from "../item/recipe.component";
   styleUrl: './users-recipes.component.css'
 })
 export class UsersRecipesComponent {
-
+  user:User|null=null;
   recipes: Recipe[] = [];
-  constructor(private recipesService: RecipesService, private router: Router) {
+  constructor(private recipesService: RecipesService,private authService: AuthService, private router: Router) {
   }
 
   ngOnInit() {
     this.loadRecipes();
-  }
+    
+     this.authService.user$.subscribe(user => {
+      this.user = user;
+    });
+  };
 
   loadRecipes() {
     this.recipesService.getMyRecipes().subscribe({
@@ -29,6 +35,30 @@ export class UsersRecipesComponent {
     console.log(this.recipes);
   }
   createRecipe() {
-    console.log("Creating new recipe : TODO")
+
+    if(!this.user){
+      console.error("User is not loged in");
+      return;
+    }
+    console.log(this.user)
+    var blankRecipe:CreateBlankRecipeDTO ={
+      name:"Blank Name",
+      description:"Blank Description",
+      category_id:1,
+      user_id:this.user.id
+    }
+     this.recipesService.createRecipe(blankRecipe).subscribe({
+    next: (res) => {
+      console.log(res);
+      const createdId = res.id; // <-- here's the ID
+
+      this.router.navigate(['recipes', createdId,"edit"]);
+      
+      // Or: this.router.navigateByUrl(`/recipes/${createdId}`);
+    },
+    error: (err) => {
+      console.error("Failed to create recipe:", err);
+    }
+  });
   }
 }
